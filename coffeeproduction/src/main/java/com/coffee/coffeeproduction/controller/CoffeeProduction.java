41ab2ba52b2e8d4ade9126083b613a8cf2e97f5d
Vaishnavi-khangal <101795826+Vaishnavi-khangal@users.noreply.github.com>
2023-03-06 15:55:27 +0530

    Update CoffeeProduction.java

--- coffeeproduction/src/main/java/com/coffee/coffeeproduction/controller/CoffeeProduction.java
+++ coffeeproduction/src/main/java/com/coffee/coffeeproduction/controller/CoffeeProduction.java
@@ -13,17 +13,18 @@ import com.coffee.coffeeproduction.entity.Coffee;
 
 @RestController
 public class CoffeeProduction {
-	
+
 	private List<Coffee> coffee = new ArrayList<>();
 
 	public CoffeeProduction()
+		//adding coffee to the list
 	{
 	
 		coffee.addAll(List.of(new Coffee(200, "Americano"), new Coffee(300, "Iced"), new Coffee(450, "Lungo"),
 				new Coffee(500, "Latte"), new Coffee(350, "Brewed coffee"), new Coffee(550, "Flat White")));
 		
 	}
-	
+	//To display all the coffee in the list
 	@RequestMapping(value = "/coffee", method = RequestMethod.GET)
 	Iterable<Coffee> getAllLeds()
 	{
