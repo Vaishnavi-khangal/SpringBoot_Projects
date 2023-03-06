5d4c75d0474c015f98133040d7e6f01d06657025
Vaishnavi-khangal <101795826+Vaishnavi-khangal@users.noreply.github.com>
2023-03-06 13:40:02 +0530

    Add files via upload

---
+++ AnudipLink/src/test/java/com/Anudip/AnudipLink/AnudipLinkApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Anudip.AnudipLink;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AnudipLinkApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
