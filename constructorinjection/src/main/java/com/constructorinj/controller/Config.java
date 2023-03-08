f726a3f71af7be93ee20e263839ad9fb47a0bfd4
Vaishnavi-khangal <101795826+Vaishnavi-khangal@users.noreply.github.com>
2023-03-08 21:05:45 +0530

    Update Config.java

--- constructorinjection/src/main/java/com/constructorinj/controller/Config.java
+++ constructorinjection/src/main/java/com/constructorinj/controller/Config.java
@@ -5,9 +5,13 @@ import org.springframework.context.annotation.Configuration;
 
 import com.constructorinj.entity.Subject;
 import com.constructorinj.entity.Trainer;
+
+//configuration annotation indicates that the class @Bean definition methods
 @Configuration
+
+//To define subject and trainer class
 public class Config {
-	//Spring @Bean Annotation is applied on a method to specify that it returns a bean to be managed by Spring context
+	// @Bean Annotation is applied on a method to specify that it returns a bean to be managed by Spring context
 	@Bean
 	public Subject subject ()
 	{
